bf05fc1c7e035a5116a7345fb47b70fd8db87761
Jonghyun Kim <jhkim@dit.ac.kr>
2019-03-21 16:50:36 +0900

    GitHub 전송

--- ButtonFun02/ViewController.swift
+++ ButtonFun02/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var labelB: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var count = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //myLabel.text = "잘 되네 하하!"
+        counterLabel.text = String(count)
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        count = count + 1
+        myLabel.text = "잘 되네 하하!"
+        counterLabel.text = String(count)
+    }
+    
+    @IBAction func buttonBpressed(_ sender: Any) {
+        //myLabel.text = "Button B pressed"
+        labelB.text = "Button B pressed!"
+    }
 }
 
